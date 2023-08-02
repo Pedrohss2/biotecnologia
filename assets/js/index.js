19e9d774ff74b2bd9ce5b2440f4b51973c28af28
PedroH <pedro2266809ph>
2023-08-02 17:57:32 -0300

    Adiciona secao de perguntas

---
+++ assets/js/index.js
@@ -0,0 +1,20 @@
+function escopo() {
+    let form = document.querySelector('.form')
+    let resultado = document.querySelector('.resposta');
+
+    const arrayDePergunta = [];
+    function  recebeEvento(event) {
+        event.preventDefault();
+        let question = document.querySelector('#pergunta');
+       
+
+        arrayDePergunta.push({
+            pergunta: question.value
+        });       
+        
+        resultado.innerHTML += `${pergunta.value}`
+    }
+
+    form.addEventListener('submit', recebeEvento)    
+}
+escopo();
